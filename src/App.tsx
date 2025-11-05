3395275224995da6780a31afbc73e2429ebe175a
Caique Jeronimo <caiquejeronimo@outlook.com>
2025-11-04 23:41:41 -0300

    using scss now instead of pure css just out of curiosity

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Olá Mundo</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nam a
+        inventore consectetur tenetur perferendis quis consequuntur harum
+        voluptas mollitia, nesciunt minus dicta minima aspernatur iusto eum
+        nobis odio sequi!
       </p>
     </>
-  )
+  );
 }
-
-export default App
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+export const module = {
+  rules: [
+    {
+      test: /\.s[ac]ss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+  ],
+};
